f10c53397b7acb00bdad1fde0afb081b53db569f
Harrison Roberts <harrison.roberts@edu.sait.ca>
2019-01-10 21:53:44 -0700

    Added a couple more features to demo

--- src/main/java/com/roberts/demo/com/roberts/restControllers/TestService.java
+++ src/main/java/com/roberts/demo/com/roberts/restControllers/TestService.java
@@ -5,6 +5,7 @@ import com.roberts.demo.com.roberts.domain.Blog;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -26,6 +27,8 @@ public class TestService {
 	 * Creates an autowired blog repository (blog is just bullshit database stuff. doesn't matter what as long
 	 * as you know this is a class that connects to the database.)
 	 *
+	 * This should be done in a constructor but this is simpler for a demo.
+	 *
 	 * The autowired annotation takes the blogRepo Interface and automagically generates a proper class with all of the
 	 * methods defined in the interface. Each method defined in the interface is a query. These methods are then made
 	 * available to this class.
@@ -34,7 +37,8 @@ public class TestService {
 	BlogRepository blogRepo;
 
 	/**
-	 * A method that responds to get requests at '/blog'
+	 * A method that responds to get requests at '/blog'. This method uses the GetMapping annotation rather than the
+	 * RequestMapping annotation. They both accomplish effectively the same thing
 	 *
 	 * @return list of blog posts as gotten from the database. These posts are Blog objects represented as JSON
 	 */
@@ -42,6 +46,33 @@ public class TestService {
 	public List<Blog> index() {
 		//Gets a list of all blog objects and returns them to the browser
 		return blogRepo.findAll();
+	}
+
+
+	/*
+	The point of the next two methods is to demonstrate that two methods can exist that respond to requests at the same
+	path. While both of the following methods respond to requests at localhost:8080/test, one method responds to GET
+	requests, and the other method responds to POST requests.
+	 */
+
+	/**
+	 * A method that responds to get requests at '/test'
+	 *
+	 * @return String
+	 */
+	@RequestMapping(value = "/test", method = RequestMethod.GET, produces = "text/plain")
+	public String listenGet() {
+		return "GET at test OK";
+	}
+
+	/**
+	 * A method that responds to post requests at '/test'
+	 *
+	 * @return String
+	 */
+	@RequestMapping(value = "/test", method = RequestMethod.POST, produces = "text/plain")
+	public String listenPost() {
+		return "POST at test OK";
 	}
 
 }
